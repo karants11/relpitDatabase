59484b24c91f1f2513f99ffad383b8d75a2a9530
Karan Rai <karants11@gmail.com>
2020-11-04 12:07:30 +0000

    editing

--- main.swift
+++ main.swift
@@ -1,15 +1,102 @@
+
+class Person {
+  let name: String
+  var age: Int
+
+  init(name: String, age: Int) {
+    self.name = name
+    self.age = age
+  }
+}
+
+class Staff: Person {
+  let isTeaching: Bool
+  var designation: String
+  let qualification: String
+  
+  init(name:String, age: Int, isTeaching: Bool, designation: String, qualification: String) {
+    self.isTeaching = isTeaching
+    self.designation = designation
+    self.qualification = qualification
+    super.init(name: name, age: age)
+  }
+}
+
+class Faculty: Staff {
+  let department: String
+  var handlingSubjects: [String]
+  var isMentor: Bool
+  var menteeList = [Student]
+  var anySpecialRole: String?
+  var experience: Float
+
+  init(name: String, age: Int, department: String, handlingSubjects: [String], isMentor: Bool, menteeList = [Student], anySpecialRole: String?, experience: Float) {
+    self.department = department
+    self.handlingSubjects = handlingSubjects
+    self.isMentor = isMentor
+    self.menteeList = menteeList
+    self.experience = experience
+    if let temp = anySpecialRole {
+				anySpecialRole = temp
+    }
+    super.init(name: name, age: age)
+  }
+
+  func guideMentees() {
+    // menteeList is the list of students required only is isMentor true
+    // guide and mentor mentees
+    // keep track on their performance
+  }
+
+  func evaluateExamPapers(sutdents: [Student]) {
+    // evaluate exam sheets of individual students and allot marks
+    // giving CE marks for individual Student
+  }
+
+  func handleSubject() {
+    // handle subject to the appropriate year as in handingSubjects
+  }
+}
+
+class Student: Person {
+	let usnNumber: String
+  var currentCGPA: Float
+  let libraryCardNumber: String
+
+  init(name: String, age: Int, usnNumber: String, currentCGPA: Float, libraryCardNumber: String) {
+    self.usnNumber = usnNumber
+    self.currentCGPA = currentCGPA
+    self.libraryCardNumber = libraryCardNumber
+    super.init(name: name, age: age)
+  }
+
+  func writeAssignments() {
+    // write assignments
+  }
+
+  func takeTest() {
+    // write Internals
+  }
+}
+
 class College {
   
-  var collegeID: Int
+  var name: String
+  var collegeID: String
   var principalName: String
   var details: String
-  var Branches = [Branch]()
-  var staffs = [staff]()
-  var students = [student]()
+  var branches: [Branch]
+  var staffs: [Staff]
+  var students: [Student]
   
-  init() {
-
-
+  init(name: String, collegeID: String, principalName: String, details: String, branches: [Branch], staffs: [Staff], students: [Student]) {
+      
+    self.name = name
+    self.collegeID = collegeID
+    self.principalName = principalName
+    self.details = details
+    self.branches = branches
+    self.students = students
   }
 
   func admitStudent(student: Student) {
@@ -39,52 +126,62 @@ class College {
   }
 }
 
-class Person {
-  let name: String
-  var age: Int
+class Branch {
+  var headOfDepartment: String
+  var students: [String: Student]
+  var staffs: [Staff]
 
-  init(name: String, age: Int) {
-    self.name = name
-    self.age = age
+  init(headOfDepartment: String, students: [String: Student], staffs: [Staff] ){
+    self.headOfDepartment = headOfDepartment
+    self.students = students
+    self.staffs = staffs
   }
+  func setTimetable() {
+    // set timetable for all the years
+    // faculty handingSubjectsmust match for perticular subjct and max 2 subject for a faculty
+  }
+   
+  func condutExam() {
+    // question papaer should be checked and distributed for all the students
+  }
+
+  func attandaceDetail(student: Student) {
+    // attandacedetail every student should be stored
+    // Student have only access to read the data not modify
+  }
+
+
+
 }
 
-class Staff: Person {
-  let isTeaching: Bool
-  var designation: String
-  let qualification: String
-  
-  init(name:String, age: Int, isTeaching: Bool, designation: String, qualification: String) {
-    self.isTeaching = isTeaching
-    self.designation = designation
-    self.qualification = qualification
-    super.init(name: name, age: age)
+class University: College {
+  var listOfCollages: [String: Collage]
+
+
+  func setSemesterTimetable() {
+    // set semester timetable of all the branches and courses
   }
+
+  func paperEvaluation() {
+    // paper evaluation of all the students and collages that comes nder the universtiy
+    // faculties from all the collages are appended to duty based on ther domail and experience
+  }
+
+  func resultPublishing() {
+    // result of all the students in the University is published branchwise or region wise or year wise
+    // results can only be viewed by the students with their usn number cannot be modified 
+  }
+
+  func assigningOfUSNNumber(student: Student) {
+    //each student is assigned with an unique USN
+  }
+
 }
 
-class Faculty: Staff {
-  let department: String
-  var handingSubjects: [String]
-  var isMentor: Bool
-  var menteeList = [Student]
+let student001 = Student(name: "ABD", age: 21,  usnNumber: "4su16ec001", currentCGPA: 8.0, libraryCardNumber: "001A")
+let student002 =  Student(name: "MNO", age: 21,  usnNumber: "4su16ec002", currentCGPA: 8.8, libraryCardNumber: "002A")
 
-  init(name: String, age: Int, department: String, handingSubjects: [String], isMentor: Bool, menteeList = [Student]) {
-    self.department = department
-    self.handingSubjects = handingSubjects
-    self.isMentor = isMentor
-    self.menteeList = menteeList
-    super.init(name: name, age: age)
-  }
+let staff1 = Staff(name: "XYZ", age: 35, isTeaching: false, designation: "Lab Assistant", qualification: "Degree")
+let staff1 = Staff(name: "MKJ", age: 45, isTeaching: false, designation: "Lab supervisor", qualification: "Degree")
 
-  func guideMentees() {
-    // menteeList is the list of students required
-    // guide and mentor mentees
-    // keep track on their performance
-  }
-
-  func evaluateExamPapers(sutdents: [Student]) {
-    // evaluate exam sheets of individual students and allot marks
-    // giving CE marks for individual Student
-  }
-  
-}
+let faculty1 = Faculty(name: "Dr. ABC", age: 42, department: "EC", handlingSubjects: ["NA", ], isMentor: Bool, menteeList = [Student], anySpecialRole: String?, experience: Float)
